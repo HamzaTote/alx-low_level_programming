8c692c5fbc975cd5879137188364d462b9f964c9
Hamzatote <hamzatote13@gmail.com>
2023-06-16 16:28:47 -0700

    qe

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - a program that prints the alphabet
+ * Return: (0) Success
+ */
+int main(void)
+{
+	int n = 97;
+	int m = 65;
+
+	while (n <= 122)
+	{
+		putchar(n);
+		if (n == 113 || n == 101)
+		{
+			n++;
+		}
+		n++;
+	}
+	putchar('\n');
+	return (0);
+}
+
