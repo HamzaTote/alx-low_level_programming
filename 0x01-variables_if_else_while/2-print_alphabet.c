d371fc641c3c4438e9fe01e865c5d3c1b6953366
Hamzatote <hamzatote13@gmail.com>
2023-06-16 15:28:19 -0700

    insomnia

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - a program that prints the alphabet
+ * Return: (0) Success
+ */
+int main(void)
+{
+	char a[26] = "abcdefghijklmnopqrstuvwxyz";
+	int i;
+
+	for (i = 0; i < 2 6; i++)
+	{
+		putchar(a[i]);
+	}
+	putchar('\n');
+	return (0);
+}
+
