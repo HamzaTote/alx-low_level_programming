8c6d3f2a0917b14221307c84e5ca2c81ea734cf0
Hamzatote <hamzatote13@gmail.com>
2023-06-16 16:47:14 -0700

    numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - a program that prints single digits
+ * Return: (0) Success
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 0; i >= 9; i++)
+	{
+		printf("%d\n", i);
+	}
+	return (0);
+}
+
