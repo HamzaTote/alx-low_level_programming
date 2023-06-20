ac4b4d4230e44c664be7f53ff078ae851c18189f
Hamzatote <hamzatote13@gmail.com>
2023-06-20 15:41:49 -0700

    sometimes suffer

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,14 +1,18 @@
 #include <stdio.h>
+/**
+ * main - a function that prints alphabets
+ * Return : (0) Success
+ */
 
 void print_alphabet(void)
 {
 	char letter = 'a';
 
-	while(letter <= 'z')
+	while (letter <= 'z')
 	{
 		putchar(letter);
 		letter++;
 	}
-	putchar('\n');
+	putchar ('\n');
 }
 
