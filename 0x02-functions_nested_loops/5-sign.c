3af7d0a6b51f81977487ffebf081fad5822cbdda
Hamzatote <hamzatote13@gmail.com>
2023-06-21 16:09:18 -0700

    sign

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -10,17 +10,17 @@ int print_sign(int n)
 {
 	if (n > 0)
 	{
-		printf("+");
+		return (+);
 		return (1);
 	}
 	else if (n == 0)
 	{
-		printf("0");
+		return(0);
 		return (0);
 	}
 	else
 	{
-		printf("-1");
+		return(-);
 		return (-1);
 	}
 }
