16fd355f344a9ba18f30e6b396501c8529be1028
Hamzatote <hamzatote13@gmail.com>
2023-06-21 06:45:55 -0700

    10x alphabet

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -10,7 +10,7 @@ void print_alphabet_x10(void)
 	char letter = 'a';
 	int i;
 
-	for (i = 0; i < 10; i++)
+	while (i < 10)
 	{
 		while (letter <= 'z')
 		{
@@ -18,7 +18,7 @@ void print_alphabet_x10(void)
 			letter++;
 		}
 		putchar('\n');
+		i++;
 	}
-	
 }
 
