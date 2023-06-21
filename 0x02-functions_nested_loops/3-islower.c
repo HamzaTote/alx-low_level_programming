6251ef711d7f7698dfc015ea34e3a534494ddd62
Hamzatote <hamzatote13@gmail.com>
2023-06-21 15:16:24 -0700

    islower

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -2,12 +2,13 @@
 
 /**
  *_islower - a function that check if letter is lower case or not
- *Description : this is a function that return 1 if lower case and 0 if not 
+ *Description : this is a function that return 1 if lower case and 0 if not
+ *Return: (1) Success (0) Fail 
  */
 
 int _islower(int c)
 {
-	if(c >= 97 && c <= 122)
+	if (c >= 97 && c <= 122)
 	{
 		return (1);
 	}
