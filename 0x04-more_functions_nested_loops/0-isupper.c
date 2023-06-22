1dc4d1ab7bcd65d83c9bc69f886a3dd88e04c8dd
Hamzatote <hamzatote13@gmail.com>
2023-06-22 12:19:41 -0700

    isupper

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -3,8 +3,11 @@
 /**
  * _isupper - a function that checks if uppercase or not
  *
+ * @c : char to check
+ *
  * Return: (1) Success (0) fail
  */
+
 int _isupper(int c)
 {
 	if (c >= 65 && c <= 90)
